48261e56cdd41a413952a6e9c8f0acbcc7ca4c0c
Kathleen Santos <kathleen.crissantos15@gmail.com>
2023-10-16 23:17:34 -0300

    Clear base project and start struct

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import styles from './Header.module.css'
+
+export default function header () {
+    return (
+        <div className={styles.backHeader}>
+            <h2>Ignite feed</h2>
+        </div>
+    )
+}
