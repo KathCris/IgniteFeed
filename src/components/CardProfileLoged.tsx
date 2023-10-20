b9df4c156984379631cada847f92ba2c42a45473
KathleenSantos <kathleen.cristine@incentive.me>
2023-10-19 22:10:56 -0300

    Style: create style card profile

--- src/components/CardProfileLoged.tsx
+++ src/components/CardProfileLoged.tsx
@@ -1,9 +1,23 @@
+import {Pencil} from "@phosphor-icons/react"
 import './CardProfileLoged.css'
+import imgBackgorund from '../assets/BackgoundProfile.png'
   
 export default function CardProfileLoged () {
   return (
     <div className='globalCardProfile'>
-      <h1>teste</h1>
+      <div className="imgBackgroundCardProfile">
+        <img src={imgBackgorund} alt="Imagem de fundo do card de perfil" />
+      </div>
+      <div className="textCardProfile">
+        <h4>Leslie Alexander</h4>
+        <p>UI Designer</p>
+      </div>
+      <div className="buttonCardProfile">
+      <button>
+        <Pencil size={32} />
+        Editar seu perfil
+      </button>
+      </div>
     </div>
   )
 }
